e84f23e8a94668f147ce75ad63ad0b443d1aede5
Manas Buzruk <66116717+ManasVB@users.noreply.github.com>
2024-10-28 15:49:55 -0400

    Bimodal predictor (#1)
    * wip
    
    * wip
    
    * bimodal done
    
    * print fix

---
+++ inc/sim_bp.h
@@ -0,0 +1,43 @@
+#ifndef SIM_BP_H
+#define SIM_BP_H
+
+#include <cstdint>
+#include <iostream>
+
+typedef struct bp_params{
+    unsigned long int K;
+    unsigned long int M1;
+    unsigned long int M2;
+    unsigned long int N;
+    char*             bp_name;
+}bp_params;
+
+// Put additional data structures here as per your requirement
+
+class BranchPredictor {
+    private:
+
+    uint32_t m;
+    uint32_t n;
+    uint32_t PT_len;  // Prediction Table length
+
+    public:
+
+    uint32_t num_predictions;
+    uint32_t num_mispredictions;
+
+    uint8_t *prediction_table;
+    std::string predictor;
+
+    BranchPredictor() = default;
+    ~BranchPredictor() = default;
+
+    uint32_t parseBranchPC(uint32_t, uint32_t);
+
+    void BP_Init(uint32_t, uint32_t);
+    void Print_Contents(void);
+
+    void Impl_Bimodal(uint32_t, bool);
+};
+
+#endif
